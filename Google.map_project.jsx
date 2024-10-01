399f186ac27bfc15d6dc12e535fbd4f0a1c2ccbc
SIRAJ <148819830+siraj0720@users.noreply.github.com>
2024-10-02 04:05:34 +0530

    Add files via upload

---
+++ Google.map_project.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <address id='address'>
+        abes  engineering college <br/>
+        ghaziabad ,up 201009
+      </address>
+      <div className='responsive-map '>  
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.616255658945!2d77.44411887123934!3d28.63297357437322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cee22c60837b7%3A0x7c35343eceb7bde0!2sABES%20Engineering%20College!5e1!3m2!1sen!2sin!4v1727820450944!5m2!1sen!2sin" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
